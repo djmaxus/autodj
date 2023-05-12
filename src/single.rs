217721eca4e25a324a2d86f44f60626442a921bf
djmaxus <djmaxus95@gmail.com>
2023-05-12 23:17:09 +0300

    still working on Dual trait
    Signed-off-by: djmaxus <djmaxus95@gmail.com>

--- src/single.rs
+++ src/single.rs
@@ -9,7 +9,7 @@ pub use crate::fluid::Dual;
 /// let x0 : DualNumber = 1.0.into(); // Parameter
 /// let x = 3.0.into_variable();
 /// let f = (x - x0).powf(2.0);
-/// assert_eq!(f.value(), 4.);
+/// assert_eq!(f.value(), &4.);
 /// assert_eq!(f.deriv(), 4.);
 /// ```
 pub type DualNumber = Common<f64>;
@@ -23,7 +23,7 @@ impl DualNumber {
     /// Get the derivative (dual component)
     #[must_use]
     pub fn deriv(&self) -> f64 {
-        self.dual()
+        self.dual().to_owned()
     }
     /// Evaluate a function over the dual number
     pub fn eval<Out>(&self, func: impl Fn(DualNumber) -> Out) -> Out {
