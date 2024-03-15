9ccf4da15a5a1507532c73729a931215aaa94d00
djmaxus <44438314+djmaxus@users.noreply.github.com>
2024-03-15 15:49:59 +0000

    fix: :rotating_light: Satisfy lints moved to `Cargo.toml`

--- examples/pendulum_sparse.rs
+++ examples/pendulum_sparse.rs
@@ -1,3 +1,10 @@
+#![allow(
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::default_numeric_fallback,
+    clippy::indexing_slicing
+)]
+
 fn main() -> Result<(), Box<dyn Error>> {
     let kappa = 1.;
     let x0 = [PI * 0.25, 0.].into_s_vector::<f64>();
@@ -6,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let x0_dual = x0.into_s_vector::<Dual2>();
 
-    let x_approx: V2<f64> = x0 + calc_x_dot(x0, kappa) * dt;
+    let x_approx: V2<f64> = x0 + calc_x_dot(&x0, kappa) * dt;
 
     let calc_residual_problem = |x0: &V2<Dual2>, x_approx: &V2<Dual2>| {
-        calc_residual(kappa, [x0.to_owned(), x_approx.to_owned()], dt, ode_scheme)
+        calc_residual(kappa, &[x0.to_owned(), x_approx.to_owned()], dt, ode_scheme)
     };
 
     let calc_residual_time_step = |x: &V2<Dual2>| calc_residual_problem(&x0_dual, x);
 
     let x1 = newton_iterations(calc_residual_time_step, &x_approx, 10, 1e-3);
 
-    dbg!(x0);
-    dbg!(x1);
+    println!("x0 = {x0:?}");
+    println!("x1 = {x1:?}");
 
     Ok(())
 }
 
-use autodj::{fluid::Dual, solid::sparse::uuid::*};
+use autodj::prelude::uuid::*;
 use nalgebra::{base::Scalar, vector, ArrayStorage, SMatrix, SVector};
 use std::{
     error::Error,
@@ -73,7 +80,7 @@ fn v_dot<T: RealOps>(u: &T, kappa: f64) -> T {
     u.sin() * T::from(kappa)
 }
 
-fn calc_x_dot<T: RealOps>(x: V2<T>, kappa: f64) -> V2<T> {
+fn calc_x_dot<T: RealOps>(x: &V2<T>, kappa: f64) -> V2<T> {
     vector![u_dot(&x[1]), v_dot(&x[0], kappa)]
 }
 
@@ -109,7 +116,7 @@ enum OdeScheme {
 }
 
 impl OdeScheme {
-    pub fn approx<T: RealOps>(&self, x: [V2<T>; 2]) -> V2<T> {
+    fn approx<T: RealOps>(&self, x: &[V2<T>; 2]) -> V2<T> {
         match self {
             Self::EulerExplicit => x[0].clone(),
             Self::EulerImplicit => x[1].clone(),
@@ -118,13 +125,13 @@ impl OdeScheme {
         }
     }
 
-    fn interpolate<T: RealOps>(x: [V2<T>; 2], weight: f64) -> V2<T> {
+    fn interpolate<T: RealOps>(x: &[V2<T>; 2], weight: f64) -> V2<T> {
         &x[0] * T::from(1.0 - weight) + &x[1] * T::from(weight)
     }
 }
 
-fn calc_residual<T: RealOps>(kappa: f64, x: [V2<T>; 2], dt: f64, ode_scheme: OdeScheme) -> V2<T> {
-    calc_x_dot_numeric(&x[0], &x[1], dt) - calc_x_dot(ode_scheme.approx(x), kappa)
+fn calc_residual<T: RealOps>(kappa: f64, x: &[V2<T>; 2], dt: f64, ode_scheme: OdeScheme) -> V2<T> {
+    calc_x_dot_numeric(&x[0], &x[1], dt) - calc_x_dot(&ode_scheme.approx(x), kappa)
 }
 
 trait IntoSVector<Input, const N: usize> {
@@ -151,7 +158,7 @@ where
     let tolerance = tolerance.abs();
 
     let mut x = x_approx.to_owned();
-    let mut error = Option::None;
+    let mut error = None;
 
     for _ in 0..num_iterations {
         let vars = x.map(|v: f64| v.into_variable());
